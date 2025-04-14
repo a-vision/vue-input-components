132eeb6a334881de923472f1a91cef5af7733327
Arnold Velzel <info@a-vision.software>
2025-04-14 09:19:12 +0200

    update for packaging

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
 import TextInput from './components/TextInput.vue'
 import FileUpload from './components/FileUpload.vue'
 import Navigation from './components/Navigation.vue'
+import Action from './components/Action.vue'
 
-export { TextInput, FileUpload, Navigation }
+export { TextInput, FileUpload, Navigation, Action }
 
 export * from './types'
